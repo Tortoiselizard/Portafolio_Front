d56d2a4f8e22b5bf2fa54ea32e10d2e57e62eebd
Troy Pernía <troy00pernia@gmail.com>
2023-07-17 15:04:06 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App () {
+  const person = {
+    name: 'Troy',
+    lastName: 'Pernía'
+  }
+
+  console.log(person)
+
+  return (
+    <>
+      <h1>Hola mundo!</h1>
+    </>
+  )
+}
+
+export default App
