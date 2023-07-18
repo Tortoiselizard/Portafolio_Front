590b1fe670de06b5bf3b0b89e075ceef5bebcffb
Troy Pernía <troy00pernia@gmail.com>
2023-07-18 11:32:34 -0400

    change components/AboutMe: add p tags (line 6-10)

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -3,7 +3,11 @@ import style from './AboutMe.module.css'
 function AboutMe () {
   return (
     <section className={style.AboutMeContainer}>
-      <h1>Estoy en AboutMe</h1>
+      <p>En marzo del 2022 un amigo me recomendó hacer un Bootcamp online de desarrollo web y no lo dudé ni un segundo. Cuatro meses despues pude dar mis primeros pasos con los lenguajes: HTML, CSS y JavaScript.</p>
+      <br />
+      <p>Aunque mi formación en ingeniería mecánica me ayudó mucho a lograr mis primeras metas, no fue hasta Octubre de ese mismo año que pude empezar el Bootcamp online y me di cuenta de lo mucho que me gustaba la programación. Y fué necesario ese impulso para poder sobrellevar la exigencia del Bootcamp, que tuvo una extención de 4 meses.</p>
+      <br />
+      <p>Finalmente, el mes de Febrero de este año pude terminar mi formación como desarrollador web Full Stack y graduarme en el Bootcamp. Pero también me di cuenta que el aprendizaje en el desarrollo web no termina nunca y que mi camino apenas empezaba.</p>
     </section>
   )
 }
