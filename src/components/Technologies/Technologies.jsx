b59e3ba7b1b93db25023123bc719fce71d6c577e
Troy Pernía <troy00pernia@gmail.com>
2023-07-18 09:53:35 -0400

    change components/Technologies: add technologies with I had worked

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -4,6 +4,28 @@ function Technologies () {
   return (
     <section className={style.TechnologiesContainer}>
       <h1>Estoy en Technologies</h1>
+      <div>
+        <ul className={style.TechnologiesFrontContainer}>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScirpt</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Bootstrap</li>
+        </ul>
+        <ul className={style.TechnologiesBackContainer}>
+          <li>NodeJS</li>
+          <li>ExpressJS</li>
+          <li>SequelizeJS</li>
+          <li>PostgreSQL</li>
+          <li>SQL</li>
+        </ul>
+
+        <ul className={style.TechnologiesOthersContainer}>
+          <li>Git</li>
+          <li>GitHub</li>
+        </ul>
+      </div>
     </section>
   )
 }
