5fc5ff1fc998ca1227897d4bd8dcb63d7c9f8bb1
Troy Pernía <troy00pernia@gmail.com>
2023-07-19 12:17:02 -0400

    change src/App: remove components: Technologies and Nav (line 14,20)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-// import Nav from './components/Nav/Nav'
 import Home from './components/Home/Home'
 import AboutMe from './components/AboutMe/AboutMe'
 import Experience from './components/Experience/Experience'
 import Projects from './components/Projects/Projects'
-import Technologies from './components/Technologies/Technologies'
 import Contact from './components/Contact/Contact'
 
 import './App.css'
@@ -13,14 +11,12 @@ function App () {
     <main className='main-Container'>
       <section className='main-fixed-Container'>
         <Home />
-        {/* <Nav /> */}
         <Contact />
       </section>
       <section className='main-scroll-Contianer'>
         <AboutMe />
         <Experience />
         <Projects />
-        <Technologies />
       </section>
     </main>
   )
