ee27f69d78b0994874d3b07417eb7616336c7be9
Troy Pernía <troy00pernia@gmail.com>
2023-07-22 19:14:19 -0400

    change components/AboutMe: add id to section (line 5)

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,7 +2,7 @@ import style from './AboutMe.module.css'
 
 function AboutMe () {
   return (
-    <section className={style.AboutMeContainer}>
+    <section id='AboutMe' className={style.AboutMeContainer}>
       <p>En marzo del 2022 un amigo me recomendó hacer un Bootcamp online de desarrollo web y no lo dudé ni un segundo. Cuatro meses despues pude dar mis primeros pasos con los lenguajes: HTML, CSS y JavaScript.</p>
       <br />
       <p>Aunque mi formación en <strong><span>ingeniería mecánica</span></strong> me ayudó mucho a lograr mis primeras metas, no fue hasta octubre de ese mismo año que pude empezar mis estudios formales en el desarrollo web y me di cuenta de lo mucho que me gustaba la programación. Ese impulso fué necesario para poder sobrellevar las exigencias del programa, que tuvo una extención de 4 meses.</p>
