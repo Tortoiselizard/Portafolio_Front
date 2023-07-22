c13a69bf43e25cd9d00b70a057bac63ffc4cb36f
Troy Pernía <troy00pernia@gmail.com>
2023-07-22 13:06:01 -0400

    change components/Technologies: add logos to all li tags (line 3-15, 23-40)

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,29 +1,43 @@
 import style from './Technologies.module.css'
 
+import htmlLogo from '../../img/html5LogoWhite.png'
+import cssLogo from '../../img/css3logoWhite.png'
+import javascriptLogo from '../../img/javascriptLogoWhite.png'
+import reactLogo from '../../img/reactLogoWhite.png'
+import reduxLogo from '../../img/reduxLogoWhite.png'
+import bootstrapLogo from '../../img/BootstrapLogoWhite.png'
+import nodeJSLogo from '../../img/nodejsLogoWhite.png'
+import expressLogo from '../../img/expressJSLogo.png'
+import sequelizeLogo from '../../img/sequelizeLogoWhite.png'
+import postgreSQLLogo from '../../img/postgreSQLLogoWhite2.png'
+import sqlLogo from '../../img/SQLLogoWhite.png'
+import gitLogo from '../../img/GitLogoWhite.png'
+import githubLogo from '../../img/githubLogoWhite.png'
+
 function Technologies () {
   return (
     <section className={style.TechnologiesContainer}>
       <h2>Habilidades</h2>
       <div>
         <ul className={style.TechnologiesFrontContainer}>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScirpt</li>
-          <li>React</li>
-          <li>Redux</li>
-          <li>Bootstrap</li>
+          <li><img src={htmlLogo} alt='HTML' /></li>
+          <li><img src={cssLogo} alt='CSS' /></li>
+          <li><img src={javascriptLogo} alt='JavaScript' /></li>
+          <li><img src={reactLogo} alt='React' /></li>
+          <li><img src={reduxLogo} alt='Redux' /></li>
+          <li><img src={bootstrapLogo} alt='Bootstrap' /></li>
         </ul>
         <ul className={style.TechnologiesBackContainer}>
-          <li>NodeJS</li>
-          <li>ExpressJS</li>
-          <li>SequelizeJS</li>
-          <li>PostgreSQL</li>
-          <li>SQL</li>
+          <li><img src={nodeJSLogo} alt='NodeJS' /></li>
+          <li><img src={expressLogo} alt='Express' /></li>
+          <li><img src={sequelizeLogo} alt='Sequelize' /></li>
+          <li><img src={postgreSQLLogo} alt='PostgreSQL' /></li>
+          <li><img src={sqlLogo} alt='SQL' /></li>
         </ul>
 
         <ul className={style.TechnologiesOthersContainer}>
-          <li>Git</li>
-          <li>GitHub</li>
+          <li><img src={gitLogo} alt='GIT' /></li>
+          <li><img src={githubLogo} alt='GitHub' /></li>
         </ul>
       </div>
     </section>
