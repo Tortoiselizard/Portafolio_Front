9b23685c4b7a2d242412316c983e825d1ef29bbd
Troy Pernía <troy00pernia@gmail.com>
2023-07-22 11:11:27 -0400

    change components/Projects: change content of main tag (line 17-18, 50)

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -14,10 +14,10 @@ function Projects () {
               <a href='https://novelty-books.vercel.app/'><h3>Novelty Books</h3></a>
             </header>
             <main>
-              <p>Participé en el desarrollo de la aplicación Novelty Books, un E-commers de libros físicos. Trabajé en el desarrollo de las herramientas que permiten gestionar la aplicación, facilitando al administrador la búsqueda de información almacenada en la base de datos, además de añadir un apartado de estadísticas relevantes donde se pueden visualizar gráficas y tablas referentes a los usuarios, libros, ordenes de compra y ventas de los libros.</p>
-              <p>Entre otras actividades, también participé en la integración del sistema de filtrados combinados y ordenado de los libros en el Front-end de la aplicación, pudiendo ejecutarlos individualmente o en conjunto para hacer búsquedas más precisas.</p>
+              <p>Novelty Books es un E-commerce de libros diseñado para tener acceso a una biblioteca virtual desde cualquier lugar. Con esta aplicación podrás buscar facilmente tus libros preferidos o también encontrar uno nuevo. Podrás realizar compras fácilmente y compartir tus comentarios con el resto de la comunidad y así ayudar a otros usuarios a conseguir el libro indicado. Podrás crear una cuenta personal que registrará todas tus compras y libros faboritos para buscarlos rápidamente en otra ocación.</p>
+              <p>Como administrador tendrás acceso a una serie de herramientas que te permitirán gestionar facilmente el E-commerce. Podrás añadir nuevos libros al catálogo o eliminarlos en caso de haberse agotado en el inventario. También tedras a disposición todas las ordenes de compra que se han registrado, los usuarios que las hicieron y los pedidos asociados. Además contarás con una sección de estadísticas que te ayudará a medir el progreso del negocio por medio de gráficas.</p>
             </main>
-            <footer>
+            {/* <footer>
               <ul className={style.listSkills}>
                 <li>HTML</li>
                 <li>CSS</li>
@@ -37,7 +37,7 @@ function Projects () {
                 <li>Git</li>
                 <li>GitHub</li>
               </ul>
-            </footer>
+            </footer> */}
           </div>
         </li>
         <li className={style.optionProyect}>
@@ -47,9 +47,9 @@ function Projects () {
               <a href='https://pi-dogs-front-mu.vercel.app/'><h3>PI-Dogs</h3></a>
             </header>
             <main>
-              <p>Diseñé y desarrollé una aplicación web donde puedes hacer búsquedas de diferentes razas de perros, visualizando el detalle de cada raza y consultar las características físicas y psicológicas del canino, como: temperamento, altura, años de vida, etc. En este proyecto desarrolle un sistema de filtrados combinados que te permite hacer búsquedas más precisas, además de diseñar y desarrollar un servidor que puede buscar información tanto de una API externa como de la base de datos propia.</p>
+              <p>PI-Dogs es una aplicación web diseñada para la comunidad amante de los perros. Con esta aplicción podrás buscar fácilmente la información más relevante de cada raza, como: su tamaño, peso y años de vida, temperamentos, etc. También podrás añadir nuevas razas de perro que no se encuentre registradas y en caso de que veas que alguien ha cometido un error al registrar alguna, también podrás hacer las modificaciones necesarias.</p>
             </main>
-            <footer>
+            {/* <footer>
               <ul className={style.listSkills}>
                 <li>HTML</li>
                 <li>CSS</li>
@@ -63,7 +63,7 @@ function Projects () {
                 <li>Git</li>
                 <li>GitHub</li>
               </ul>
-            </footer>
+            </footer> */}
           </div>
         </li>
       </ul>
