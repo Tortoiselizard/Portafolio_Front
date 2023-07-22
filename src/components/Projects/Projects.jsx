05bcbccff64574fa77fd72aee1c9e3544cef29ba
Troy Pernía <troy00pernia@gmail.com>
2023-07-22 09:48:14 -0400

    change components/Projects: add footer to projects (line 20-40, 52-66), and add class to li tags (line 10, 43)

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,7 +7,7 @@ function Projects () {
     <section className={style.ProjectsContainer}>
       <h2>Proyectos</h2>
       <ul>
-        <li>
+        <li className={style.optionProyect}>
           <img src={noveltyBooks} alt='Novelty-Books' />
           <div className={style.informationOptionContainer}>
             <header>
@@ -17,9 +17,30 @@ function Projects () {
               <p>Participé en el desarrollo de la aplicación Novelty Books, un E-commers de libros físicos. Trabajé en el desarrollo de las herramientas que permiten gestionar la aplicación, facilitando al administrador la búsqueda de información almacenada en la base de datos, además de añadir un apartado de estadísticas relevantes donde se pueden visualizar gráficas y tablas referentes a los usuarios, libros, ordenes de compra y ventas de los libros.</p>
               <p>Entre otras actividades, también participé en la integración del sistema de filtrados combinados y ordenado de los libros en el Front-end de la aplicación, pudiendo ejecutarlos individualmente o en conjunto para hacer búsquedas más precisas.</p>
             </main>
+            <footer>
+              <ul className={style.listSkills}>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>NextJS</li>
+                <li>Next Auth</li>
+                <li>Stripe</li>
+                <li>Chart.js</li>
+                <li>Bootstrap</li>
+                <li>Node.js</li>
+                <li>Cloudinary</li>
+                <li>Mongoose</li>
+                <li>MongoDB</li>
+                <li>ExpressJS</li>
+                <li>Nodemailer</li>
+                <li>Git</li>
+                <li>GitHub</li>
+              </ul>
+            </footer>
           </div>
         </li>
-        <li>
+        <li className={style.optionProyect}>
           <img src={piDogs} alt='PI-Dogs' />
           <div className={style.informationOptionContainer}>
             <header>
@@ -28,6 +49,21 @@ function Projects () {
             <main>
               <p>Diseñé y desarrollé una aplicación web donde puedes hacer búsquedas de diferentes razas de perros, visualizando el detalle de cada raza y consultar las características físicas y psicológicas del canino, como: temperamento, altura, años de vida, etc. En este proyecto desarrolle un sistema de filtrados combinados que te permite hacer búsquedas más precisas, además de diseñar y desarrollar un servidor que puede buscar información tanto de una API externa como de la base de datos propia.</p>
             </main>
+            <footer>
+              <ul className={style.listSkills}>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>Sequelize</li>
+                <li>PostgreSQL</li>
+                <li>Git</li>
+                <li>GitHub</li>
+              </ul>
+            </footer>
           </div>
         </li>
       </ul>
