65ab41d8af675f1f897f9ad1621cb81435170f07
Troy Pernía <troy00pernia@gmail.com>
2023-07-22 09:35:00 -0400

    change components/AboutMe: add span and strong tags into de p tag (line 8,10)

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,9 +5,9 @@ function AboutMe () {
     <section className={style.AboutMeContainer}>
       <p>En marzo del 2022 un amigo me recomendó hacer un Bootcamp online de desarrollo web y no lo dudé ni un segundo. Cuatro meses despues pude dar mis primeros pasos con los lenguajes: HTML, CSS y JavaScript.</p>
       <br />
-      <p>Aunque mi formación en ingeniería mecánica me ayudó mucho a lograr mis primeras metas, no fue hasta Octubre de ese mismo año que pude empezar el Bootcamp online y me di cuenta de lo mucho que me gustaba la programación. Y fué necesario ese impulso para poder sobrellevar la exigencia del Bootcamp, que tuvo una extención de 4 meses.</p>
+      <p>Aunque mi formación en <strong><span>ingeniería mecánica</span></strong> me ayudó mucho a lograr mis primeras metas, no fue hasta octubre de ese mismo año que pude empezar mis estudios formales en el desarrollo web y me di cuenta de lo mucho que me gustaba la programación. Ese impulso fué necesario para poder sobrellevar las exigencias del programa, que tuvo una extención de 4 meses.</p>
       <br />
-      <p>Finalmente, el mes de Febrero de este año pude terminar mi formación como desarrollador web Full Stack y graduarme en el Bootcamp. Pero también me di cuenta que el aprendizaje en el desarrollo web no termina nunca y que mi camino apenas empezaba.</p>
+      <p>Finalmente, el mes de febrero de este año pude terminar mi formación y graduarme como <strong><span>Desarrollador Web Full Stack</span></strong>. Pero también me di cuenta que el aprendizaje en el desarrollo web no termina nunca y que mi camino apenas empezaba.</p>
     </section>
   )
 }
