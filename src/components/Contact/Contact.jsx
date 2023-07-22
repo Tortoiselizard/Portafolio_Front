fc0f6fc79bbe386d45a4bd798f0238d145fec574
Troy Pernía <troy00pernia@gmail.com>
2023-07-21 20:44:19 -0400

    change components/Contact: add function copyGmail (line 4-18) and add href to a tag (line 22,23)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,27 @@
 import style from './Contact.module.css'
 
 function Contact () {
+  function copyGmail () {
+    const footer = document.getElementsByName('footerContainer')[0]
+    const textArea = document.createElement('input')
+    const message = document.createElement('lable')
+    textArea.value = 'troy00pernia@gmail.com'
+    message.textContent = 'Copiado'
+    footer.appendChild(textArea)
+    textArea.select()
+    document.execCommand('copy')
+    textArea.remove()
+    footer.appendChild(message)
+    message.classList.add(style.labelGmail)
+    setTimeout(() => message.remove(), 1000)
+  }
+
   return (
-    <footer className={style.ContactContainer}>
+    <footer name='footerContainer' className={style.ContactContainer}>
       <ul>
-        <li><a /></li>
-        <li><a /></li>
-        <li><a /></li>
+        <li><a href='https://github.com/Tortoiselizard' /></li>
+        <li><a href='https://www.linkedin.com/in/troy-dj-pernia/' /></li>
+        <li><a onClick={copyGmail} /></li>
       </ul>
     </footer>
   )
