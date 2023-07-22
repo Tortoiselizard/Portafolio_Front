72c2bf5c7ed4d6733c07288bfc1f2fe524daa9be
Troy Pernía <troy00pernia@gmail.com>
2023-07-22 19:16:26 -0400

    change components/Nav: add function scrollToSection (line 4-8)

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,34 @@
 import style from './Nav.module.css'
 
 function Nav () {
+  function scrollToSection (sectionId) {
+    const section = document.getElementById(sectionId)
+    section.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <nav className={style.Nav}>
       <ul>
         <li>
-          <a>
+          <a onClick={() => scrollToSection('AboutMe')}>
             <div />
             <label>Sobre mi</label>
           </a>
         </li>
         <li>
-          <a>
+          <a onClick={() => scrollToSection('Experience')}>
             <div />
             <label>Experiencia</label>
           </a>
         </li>
         <li>
-          <a>
+          <a onClick={() => scrollToSection('Projects')}>
             <div />
             <label>Proyectos</label>
           </a>
         </li>
         <li>
-          <a>
+          <a onClick={() => scrollToSection('Technologies')}>
             <div />
             <label>Habilidades</label>
           </a>
