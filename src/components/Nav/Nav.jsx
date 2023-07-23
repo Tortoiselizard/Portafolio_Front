37d6c930f54d1f1de887fa726a5ce60c513be772
Troy Pernía <troy00pernia@gmail.com>
2023-07-22 20:30:42 -0400

    change components/Nav: add useEffect (line 10-27). Add name to a tags (line 44, 50, 56), and comment last li tag (line 61-66)

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,10 +2,29 @@ import { useEffect } from 'react'
 
 import style from './Nav.module.css'
 
+const arraySections = ['#AboutMe', '#Experience', '#Projects']
+
 function Nav () {
   useEffect(() => {
     const handleScroll = () => {
-      console.log('Scrolling...')
+      const objectSections = {}
+      arraySections.map(section => document.querySelector(section)).forEach((section, index) => {
+        objectSections[arraySections[index].slice(1)] = section.getBoundingClientRect()
+      })
+      function selectCurrentSection (objectSections) {
+        arraySections.forEach(section => {
+          const sectionCurrent = document.getElementsByName(section.slice(1))[0]
+          const currentDiv = sectionCurrent.querySelector('div')
+          if (objectSections[section.slice(1)].top <= 100 && objectSections[section.slice(1)].bottom > 100) {
+            sectionCurrent.classList.add(style.currentSectionA)
+            currentDiv.classList.add(style.currentSectionDiv)
+          } else {
+            sectionCurrent.classList.remove(style.currentSectionA)
+            currentDiv.classList.remove(style.currentSectionDiv)
+          }
+        })
+      }
+      selectCurrentSection(objectSections)
     }
     window.addEventListener('scroll', handleScroll)
     return () => {
@@ -22,29 +41,29 @@ function Nav () {
     <nav className={style.Nav}>
       <ul>
         <li>
-          <a onClick={() => scrollToSection('AboutMe')}>
+          <a name='AboutMe' onClick={() => scrollToSection('AboutMe')}>
             <div />
             <label>Sobre mi</label>
           </a>
         </li>
         <li>
-          <a onClick={() => scrollToSection('Experience')}>
+          <a name='Experience' onClick={() => scrollToSection('Experience')}>
             <div />
             <label>Experiencia</label>
           </a>
         </li>
         <li>
-          <a onClick={() => scrollToSection('Projects')}>
+          <a name='Projects' onClick={() => scrollToSection('Projects')}>
             <div />
             <label>Proyectos</label>
           </a>
         </li>
-        <li>
-          <a onClick={() => scrollToSection('Technologies')}>
+        {/* <li>
+          <a name='Technologies' onClick={() => scrollToSection('Technologies')}>
             <div />
             <label>Habilidades</label>
           </a>
-        </li>
+        </li> */}
       </ul>
     </nav>
   )
