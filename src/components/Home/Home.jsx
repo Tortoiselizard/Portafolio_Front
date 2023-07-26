bf7f859d56f9cceff4e5914c54e81049d3a0bd21
Troy Pernía <troy00pernia@gmail.com>
2023-07-26 14:09:36 -0400

    change components/Home: add class (line 9)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home () {
     <section className={style.HomeContainer}>
       <h1>Troy Pernía</h1>
       <h2>Desarrollador Web Full Stack</h2>
-      <p>Apacionado de la salud, educación y del <strong><span>Desarrollo Web</span></strong></p>
+      <p className={style.phraseHome}>Apacionado a la salud, educación y al <strong><span>Desarrollo Web</span></strong></p>
       <Nav />
     </section>
   )
