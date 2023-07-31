c695f9ddf0ebdf3028522a96b502aaafd69cbf12
Troy Pernía <troy00pernia@gmail.com>
2023-07-31 12:36:41 -0400

    change components/Home: correct misspellings (line 9)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home () {
     <section className={style.HomeContainer}>
       <h1>Troy Pernía</h1>
       <h2>Desarrollador Web Full Stack</h2>
-      <p className={style.phraseHome}>Apacionado a la salud, educación y al <strong><span>Desarrollo Web</span></strong></p>
+      <p className={style.phraseHome}>Apasionado a la tecnología, a la mecánica y al <strong><span>Desarrollo Web</span></strong></p>
       <Nav />
     </section>
   )
