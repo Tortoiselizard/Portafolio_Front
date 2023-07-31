fee2f2c86d647d16444724e500ada575bb0407e8
Troy Pernía <troy00pernia@gmail.com>
2023-07-31 12:35:09 -0400

    change components/AboutMe: correct misspellings into p tag (line 7)

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,7 +4,7 @@ function AboutMe () {
   return (
     <section id='AboutMe' className={style.AboutMeContainer}>
       <p>El año pasado un amigo me recomendó incursionar en el mundo del <strong><span>desarrollo web</span></strong>. Después de hacer un curso corto me di cuenta que realmente me gustaba escribir código, así que dejé mi trabajo como técnico de ascensores y decidí redireccionar mi rumbo profesional.</p>
-      <p>Un año despues logré graduarme como <strong><span>Full Stack Web Developer</span></strong> y me doy cuenta que aunque mi carrera como <strong><span>Ingeniero Mecánico</span></strong> aún me sigue gustando, definitivamente he encontrado otra pación en mi vida.</p>
+      <p>Un año después logré graduarme como <strong><span>Full Stack Web Developer</span></strong> y me doy cuenta que aunque mi carrera como <strong><span>Ingeniero Mecánico</span></strong> aún me sigue gustando, definitivamente he encontrado otra pación en mi vida.</p>
     </section>
   )
 }
