8c386ec56daa1b830b1fdfc2983a72d945343ac3
Troy Pernía <troy00pernia@gmail.com>
2023-09-21 17:24:39 -0400

    change .eslintrc.cjs: comment extends (line 7-9)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,10 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    './node_modules/standard/eslintrc.json',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended'
+    './node_modules/standard/eslintrc.json'
+    // 'plugin:react/recommended',
+    // 'plugin:react/jsx-runtime',
+    // 'plugin:react-hooks/recommended'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
