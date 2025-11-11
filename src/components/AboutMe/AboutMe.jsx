ea7e58b9dd33c159bcd9fe9a9b02003328a36b25
Troy Pernía <troy00pernia@gmail.com>
2025-11-11 19:27:30 -0400

    fix: fix years experience

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -3,7 +3,7 @@ import style from './AboutMe.module.css'
 function AboutMe () {
   return (
     <section id='AboutMe' className={style.AboutMeContainer}>
-      <p>Soy <strong><span>desarrollador web fullstack</span></strong> con un año de experiencia en la creación de soluciones digitales que abordan necesidades reales. Me especializo en diseñar y desarrollar herramientas funcionales y eficaces que optimizan procesos y mejoran la experiencia del usuario.</p>
+      <p>Soy <strong><span>desarrollador web fullstack</span></strong> con dos años de experiencia en la creación de soluciones digitales que abordan necesidades reales. Me especializo en diseñar y desarrollar herramientas funcionales y eficaces que optimizan procesos y mejoran la experiencia del usuario.</p>
       <p>Además de mi interés por el <strong><span>desarrollo web</span></strong>, me entusiasma el mundo de los videojuegos y sueño con crear experiencias interactivas que combinen tecnología y creatividad.</p>
     </section>
   )
